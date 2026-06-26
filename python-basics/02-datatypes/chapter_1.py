8d73e56a097a91816a5715766fc6639362d2c6c2
Sumitraj Saroj <sumitrajsaroj01@gmail.com>
2026-06-26 13:09:25 +0530

    done with chapter 2

---
+++ python-basics/02-datatypes/chapter_1.py
@@ -0,0 +1,14 @@
+suagr_amount = 2
+print(f"Initial Sugar: {suagr_amount}")
+
+
+suagr_amount = 12
+print(f" Second Initial Sugar: {suagr_amount}")
+
+print(f"ID of 2 is : {id(2)}")
+
+
+print(f"ID of 2 is : {id(12)}")
+
+
+print("Hello World")
---
+++ python-basics/02-datatypes/chapter_2.py
@@ -0,0 +1,15 @@
+spice_mix = set()
+
+print(f"Initial Spice mix id: {id(spice_mix)}")
+
+spice_mix.add("Ginger")
+
+spice_mix.add("Cinnnemon")
+
+
+print(f"Second Initial Spice mix id: {id(spice_mix)}")
+
+
+print(spice_mix)
+
+
