2832688f322f688881a6a8968c78706f0c6deafe
Sumitraj Saroj <sumitrajsaroj01@gmail.com>
2026-06-26 14:53:53 +0530

    done with chapter 5 and 6

---
+++ python-basics/02-datatypes/chapter_5.py
@@ -0,0 +1,11 @@
+import sys
+from fractions import Fraction
+
+ideal_temp = 95.5
+current_temp = 95.49999999
+
+print(ideal_temp)
+print(current_temp)
+print(ideal_temp - current_temp)
+
+print(sys.float_info)
---
+++ python-basics/02-datatypes/chapter_6.py
@@ -0,0 +1,18 @@
+chai_type = "Ginger"
+
+customer_name = "sumit"
+
+print(f"Order for : {customer_name} : {chai_type} please!")
+
+
+chai_description = "Aromatic and blod"
+
+print(f"Frist word: {chai_description[0:8:2]}")
+
+
+print(f"Frist word: {chai_description[::-1]}")
+
+label_simple = "Sumit"
+
+encoded_string = label_simple.encode("utf-8")
+print(encoded_string)
