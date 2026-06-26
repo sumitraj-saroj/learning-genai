29dbacdd25e9f86586aa670c9509cf631e7f14fa
Sumitraj Saroj <sumitrajsaroj01@gmail.com>
2026-06-26 14:29:19 +0530

    done with chapter 3 and 4

---
+++ python-basics/02-datatypes/chapter_3.py
@@ -0,0 +1,25 @@
+# This are comments
+
+black_tea_grams = 15
+
+ginger_tea = 3
+
+total_grams = black_tea_grams + ginger_tea
+print(f"Total Grams is : {total_grams}")
+
+
+milk_litres = 10
+milk_left = 3
+
+left = milk_litres / milk_left
+print(f"This is left: {left}")
+
+
+milk_litres = 17
+milk_left = 3
+
+left = milk_litres // milk_left
+print(f"This is left: {left}")
+
+total_money = 1_000_000_000
+print(f"this is : {total_money}")
---
+++ python-basics/02-datatypes/chapter_4.py
@@ -0,0 +1,12 @@
+isLoading = True
+
+hello = 25
+
+print(f"Hello + isLoading is : {isLoading + hello}")  # Upcasting in python
+
+water_hot = True
+tea_add = False
+
+can_server = water_hot and tea_add
+
+print(f"Can Server: {can_server}")
