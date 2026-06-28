7342f8555fe7e1c389beb9c9dc0700a1d1873605
Sumitraj Saroj <sumitrajsaroj01@gmail.com>
2026-06-28 09:08:03 +0530

    complete half of the loops

---
+++ python-basics/04-loops/01-tea-token-dispenser.py
@@ -0,0 +1,3 @@
+number = 1
+for number in range(1, 11):
+    print(f"Serving Chai to Token number #{number}")
---
+++ python-basics/04-loops/03-chai-order.py
@@ -0,0 +1,4 @@
+name = ["sumit", "shrutik", "himanshu", "sholk"]
+
+for i in name:
+    print(f"Order is ready for #{i}")
