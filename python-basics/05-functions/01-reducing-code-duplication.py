b06175cacb52fe36d81c46802a2b50dedb8e747b
Sumitraj Saroj <sumitrajsaroj01@gmail.com>
2026-06-28 16:15:28 +0530

    started functions

---
+++ python-basics/05-functions/01-reducing-code-duplication.py
@@ -0,0 +1,5 @@
+def print_order(name, chai_type):
+    print(f"{name} ordered {chai_type}")
+
+
+print_order("sumit", "lemon_tea")
---
+++ python-basics/05-functions/02-cafe-sales.py
@@ -0,0 +1,20 @@
+def fetch_report():
+    print("Fetching the sales data")
+
+
+def filter_valid_data():
+    print("filtering valid sales data")
+
+
+def summarize_the_data():
+    print("summarizing the data")
+
+
+def generate_report():
+    fetch_report()
+    filter_valid_data()
+    summarize_the_data()
+    print("Report is Ready")
+
+
+generate_report()
