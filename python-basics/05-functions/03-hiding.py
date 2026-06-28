2fde3d39cbe0e8f15440edd8721d9d86005bb8d8
Sumitraj Saroj <sumitrajsaroj01@gmail.com>
2026-06-28 16:31:01 +0530

    completed functions

---
+++ python-basics/05-functions/03-hiding.py
@@ -0,0 +1,19 @@
+def get_input():
+    print("Getting user input")
+
+
+def validate_user():
+    print("Validating the user database")
+
+
+def save_to_db():
+    print("Saving the user info")
+
+
+def register_user():
+    get_input()
+    validate_user()
+    save_to_db()
+
+
+register_user()
---
+++ python-basics/05-functions/04-readability.py
@@ -0,0 +1,7 @@
+def calculate_bills(cups, price_cups):
+    return cups * price_cups
+
+
+mybill = calculate_bills(4, 200)
+
+print(mybill)
