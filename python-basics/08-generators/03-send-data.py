269a29c44b420affa629f5cacd393460d1b4ce63
Sumitraj Saroj <sumitrajsaroj01@gmail.com>
2026-06-30 21:11:08 +0530

    send data done

---
+++ python-basics/08-generators/03-send-data.py
@@ -0,0 +1,14 @@
+def chai_customer():
+    print("Welcome ! What Chai would you like")
+    order = yield
+    while True:
+        print(f"Preparing: {order}")
+        order = yield
+
+
+stall = chai_customer()
+next(stall)  # start the generator
+
+stall.send("Masala Chai")
+
+stall.send("lemon Chai")
